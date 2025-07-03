ea17ed892678d54f10b78fc9627cc98b23a28ee3
Robert Kerr <code@scanhelper.com>
2025-07-04 07:25:13 +0800

    create project skeleton

---
+++ FamilySlide.App/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace FamilySlide.App
+
+open System
+open Avalonia
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace(areas = Array.empty)
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
---
+++ FamilySlide.Core/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace FamilySlide.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
